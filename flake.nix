8f9d15fb3d6fb4f7d75d4fbe996abe156838b1b9
Preston Pan <ret2pop@nullring.xyz>
2026-03-16 20:25:25 -0700

    oops

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-25.05";
+      url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
